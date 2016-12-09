ad3128dc6da33598dc932c27f5eb8039f0cce919
Wgssmart <847315251@qq.com>
2016-12-09 14:05:53 +0800

    项目整体框架

---
+++ src/test/java/ProductParserTest.java
@@ -0,0 +1,14 @@
+
+import junit.framework.TestCase;
+
+/**
+ * Created by WangGenshen on 11/28/16.
+ */
+public class ProductParserTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+
+    }
+
+}
